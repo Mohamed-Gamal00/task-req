9637650df2b30cc830b05d0cfa38663583e116fe
Mohamed-Gamal00 <gmohamed777@yahoo.com>
2023-07-03 15:49:59 +0300

    register

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -6,7 +6,7 @@
         <div class="col-lg-6 m-0 p-0 align-self-center">
           <div class="row p-0 m-auto justify-content-center">
             <div class="col-md-10">
-              <form class="form-inline"  @submit.prevent="Register()">
+              <form class="form-inline" @submit.prevent="Register()">
                 <div class="row">
                   <!-- name -->
                   <div class="row mb-3">
@@ -85,17 +85,82 @@
                       <button style="width: 100%" class="btn text-white">إنشاء حساب</button>
                       <div class="text-center mt-lg-2">
                         <span class="fs-10 fw-bold">Have an account? login</span>
-                        <button
-                          class="btn text-purple fs-10 fw-bold cairo"
-                          :to="{ name: 'login' }"
-                        >
-                          تسجيل دخول
+                        <button type="submit" class="btn text-purple fs-10 fw-bold cairo">
+                          Create New Account
                         </button>
                       </div>
                     </div>
                   </div>
                 </div>
               </form>
+
+              <transition name="fade">
+                <div class="modalpopup" v-if="show">
+                  <div class="modal__backdrop">
+                    <div class="modal__dialog">
+                      <div class="modal__body">
+                        <slot name="body" />
+                        <div class="">
+                          <div>
+                            <form @submit.prevent="verify()">
+                              <!-- header -->
+                              <div class="d-inline">
+                                <h5 class="fw-bold text-center" style="color: #322a7d">
+                                  <!-- header -->
+                                  <div class="d-inline">
+                                    <h5 class="fw-bold text-center" style="color: #322a7d">
+                                      هيا لنبدأ
+                                    </h5>
+                                    <p class="text-center">تم ارسال الكود الي الرقم المسجل</p>
+                                    <p>+5222147810004</p>
+                                  </div>
+                                </h5>
+                                <p class="text-center">رمز التأكيد</p>
+                              </div>
+                              <!-- رقم الهاتف -->
+                              <div class="row g-3 align-items-center justify-content-center">
+                                <div class="col-auto m-3" style="width: 100%">
+                                  <input
+                                    type="text"
+                                    dir="rtl"
+                                    class="form-control"
+                                    placeholder="phone"
+                                    v-model="phone"
+                                  />
+                                  <input
+                                    type="text"
+                                    dir="rtl"
+                                    class="form-control"
+                                    placeholder="phone_code"
+                                    v-model="phone_code"
+                                  />
+                                  <input
+                                    type="text"
+                                    dir="rtl"
+                                    class="form-control"
+                                    placeholder="code"
+                                    v-model="code"
+                                  />
+                                </div>
+                              </div>
+                              <!-- footer -->
+                              <div class="modal__footer text-center">
+                                <button class="btn" type="button" @click="verify_Phone()">
+                                  تاكيد
+                                </button>
+                                <button class="btn" type="button" @click="closeModal()">
+                                  اغلاق
+                                </button>
+                                <br />
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </transition>
             </div>
           </div>
         </div>
@@ -117,12 +182,17 @@ export default {
   name: 'LoginCom',
   data() {
     return {
+      show: false,
+      phonInput: false,
+
       full_name: '',
       email: '',
       password: '',
       password_confirmation: '',
       phone: '',
-      phone_code: ''
+      phone_code: '',
+
+      code: ''
     }
   },
   // mounted() {
@@ -132,23 +202,60 @@ export default {
   //   }
   // },
   methods: {
+    closeModal() {
+      this.show = false
+      document.querySelector('body').classList.remove('overflow-hidden')
+    },
+    // openModal() {
+    //   this.show = true
+    //   document.querySelector('body').classList.add('overflow-hidden')
+    // },
     async Register() {
       const credentaials = {
-        full_name:this.full_name,
-        email:this.email,
-        password:this.password,
-        password_confirmation: this.password_confirmation ,
+        full_name: this.full_name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.password_confirmation,
         phone: this.phone,
-        phone_code: this.phone_code,
+        phone_code: this.phone_code
       }
       console.log('form validated Succesfuly')
       await axios
         .post(`http://lawyer.phpv8.aait-d.com/api/client_web/register`, credentaials, {})
         .then((res) => {
           console.log(res)
+          this.show = true
+        })
+        .catch((error) => {
+          if (error.response) {
+            // Handle server error
+            console.log(error.response.data)
+            console.log(error.response.status)
+            console.log(error.response.headers)
+          } else if (error.request) {
+            // Handle network error
+            console.log(error.request)
+          } else {
+            // Handle other errors
+            console.log('Error:', error.message)
+          }
+        })
+    },
+    async verify_Phone() {
+      console.log('veryfy code process')
+      const verify_Phone = {
+        phone: this.phone,
+        phone_code: this.phone_code,
+        code: this.code
+      }
+      await axios
+        .post(`http://lawyer.phpv8.aait-d.com/api/client_web/verify_Phone`, verify_Phone)
+        .then((res) => {
+          console.log(res)
           localStorage.setItem('token', res.data.data.token)
           localStorage.setItem('user', JSON.stringify(res.data))
           setAuthHeader(res.data.token)
+          console.log('veryfy code success')
           this.$router.push({ name: 'client' })
         })
         .catch((err) => {
@@ -210,5 +317,44 @@ input::-webkit-inner-spin-button {
 /* Firefox */
 input[type='number'] {
   -moz-appearance: textfield;
+}
+
+/* popup */
+
+.modalpopup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(27, 27, 27, 0.212);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+}
+.modalpopup > div {
+  background-color: #fff;
+  padding: 50px;
+  border-radius: 10px;
+}
+.modalpopup > div .form-control {
+  color: #000000;
+  background-color: #ced4da;
+  border-radius: 12px;
+}
+.modalpopup > div .form-control:focus {
+  background-color: #ced4da;
+  border-color: #8a877b00;
+  outline: 0;
+  box-shadow: 0 0 0 0.25rem rgba(173, 157, 91, 0);
+}
+@media only screen and (min-width: 1200px) {
+  .modalpopup > div {
+    background-color: #fff;
+    padding: 50px;
+    border-radius: 10px;
+    width: 34%;
+  }
 }
 </style>
